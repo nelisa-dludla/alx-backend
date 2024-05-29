188f6cac136438c3095cf67a033e8aa8080b2258
Nelisa Dludla <nelisa.dludla@outlook.com>
2024-05-29 02:52:30 +0200

    Added solution to task 4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+'''
+A simple Flask app with Babel
+'''
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext as _
+
+
+class Config:
+    '''
+    Defines the Config class
+    '''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    '''
+    Determines the ideal language amongst the
+    supported languages
+    '''
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+
+    supported_languages = request.accept_languages.languages()
+
+    for language in supported_languages:
+        if language in app.config['LANGUAGES']:
+            return language
+
+    return app.config['BABEL_DEFAULT_LOCALE']
+
+
+@app.route('/')
+def hello_world():
+    '''
+    Renders 3-index.html
+    '''
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
